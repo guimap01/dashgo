f6f6c63c1eb181836a51ec0ef573d4953209dfd4
Pedro Guimaraes <pedromonteirogui@gmail.com>
2022-03-15 23:53:31 -0300

    feat: Header component

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import Header from 'components/Header';
+import { NextPage } from 'next';
+import React from 'react';
+
+const Dashboard: NextPage = () => {
+  return <Header />;
+};
+
+export default Dashboard;
