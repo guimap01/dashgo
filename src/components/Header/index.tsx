ecfc247fdd5cf2aed58d73cf209f5659bae59331
Pedro Guimaraes <pedromonteirogui@gmail.com>
2022-03-17 22:02:10 -0300

    refactor: Header responsivenes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export const Header = () => {
+  const isLarge = useBreakpointValue({ base: false, lg: true });
   return (
     <Flex
       as="header"
@@ -17,10 +18,10 @@ export const Header = () => {
       align="center"
     >
       <Logo />
-      <SearchBox />
+      {isLarge && <SearchBox />}
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isLarge} />
       </Flex>
     </Flex>
   );
